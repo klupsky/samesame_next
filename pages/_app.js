e8dd960ac9b361d1277c8ad2a3074a9a43b76df5
samesamestudio <talk@samesame.studio>
2021-05-21 20:49:02 +0200

    seo added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '@/styles/globals.scss';
+import { DefaultSeo } from 'next-seo';
+
+const pageTitle = 'Your page title';
+const pageDescription = 'Your page description';
+
+const defaultSeo = {
+   title: pageTitle,
+   description: pageDescription,
+   openGraph: {
+      title: pageTitle,
+      description: pageDescription,
+      type: 'website',
+      locale: 'en_IE',
+      images: [{ url: 'absoluteimageurl.jpg' }]
+   },
+   twitter: {
+      cardType: 'summary_large_image'
+   }
+};
 
 function MyApp({ Component, pageProps }) {
-   return <Component {...pageProps} />;
+   return (
+      <>
+         <DefaultSeo {...defaultSeo} />
+         <Component {...pageProps} />
+      </>
+   );
 }
 
 export default MyApp;
