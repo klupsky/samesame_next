1386e107733d3dd7b7bcbb2888d0bc1784492af1
samesamestudio <talk@samesame.studio>
2021-05-26 19:27:02 +0200

    added: custom styled component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
+import styled from 'styled-components';
+
 export default function Home() {
    return (
       <div>
-         <p>Hello World</p>
+         <Paragraph>Hello World</Paragraph>
       </div>
    );
 }
+
+// add your custom styles here:
+const Paragraph = styled.p`
+   text-align: center;
+   font-size: 30px;
+`;
