2c6e009e3ddbeecb645bb58ab79e571205151759
Same Same Studio <77662859+samesamestudio@users.noreply.github.com>
2021-07-09 13:12:54 +0200

    added: page reveal

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,31 @@ import styled from 'styled-components';
 import { device } from '@/styles/BreakPoints';
 import { Headline } from '@/styles/BaseStyles';
 
+import { motion } from 'framer-motion';
+
 export default function Home() {
    /* Add an SEO to every page */
    // const pageSeoProps = generateSeo(data?.pageSeo?.seoTitle ? data?.pageSeo : globalData?.pageSeo);
 
    return (
-      <>
+      // This adds a page transiton after routing (simple fade in)
+      <motion.div
+         initial="hidden"
+         animate="visible"
+         variants={{
+            hidden: {
+               opacity: 0
+            },
+            visible: {
+               opacity: 1
+            }
+         }}>
          {/* <NextSeo {...pageSeoProps} /> */}
          <div>
-            <Headline>I'm a headline</Headline>
+            <Headline>Headline</Headline>
             <Paragraph>Hello World</Paragraph>
          </div>
-      </>
+      </motion.div>
    );
 }
 
