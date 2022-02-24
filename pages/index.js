f5b1bf22c2449c08bcdb70ef9ccd98c8db9504c4
Same Same Studio <77662859+samesamestudio@users.noreply.github.com>
2022-02-24 18:00:46 +0100

    refactoring index

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,17 @@
 import { H2, P } from '@/styles/base/BaseStyles';
 import LayoutMain from '@/layout/layoutMain';
 
-import { motion } from 'framer-motion';
-
 export default function Home() {
    /* Add an SEO to every page */
    // const pageSeoProps = generateSeo(data?.pageSeo?.seoTitle ? data?.pageSeo : globalData?.pageSeo);
 
    return (
-      // This adds a page transiton after routing (simple fade in)
-      <motion.div
-         initial="hidden"
-         animate="visible"
-         variants={{
-            hidden: {
-               opacity: 0
-            },
-            visible: {
-               opacity: 1
-            }
-         }}>
+      <>
          {/* <NextSeo {...pageSeoProps} /> */}
          <LayoutMain>
             <H2>Headline</H2>
             <P>Hello World</P>
          </LayoutMain>
-      </motion.div>
+      </>
    );
 }
