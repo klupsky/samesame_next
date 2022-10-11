007ca42e0236f858d598372e37f39d43a539ee05
klupsky <katharina@chalupsky.eu>
2022-10-11 14:32:00 +0200

    Adding everything so i can pull it

--- components/Icons.js
+++ components/Icons.js
@@ -1,82 +1,134 @@
 export const IconSearch = (
    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-   <path d="M22.463 23.5236C22.7522 23.8202 23.227 23.8262 23.5236 23.537C23.8202 23.2478 23.8262 22.773 23.537 22.4764L22.463 23.5236ZM18.0878 9.81768C18.0878 14.2653 14.4386 17.8854 9.91892 17.8854V19.3854C15.2508 19.3854 19.5878 15.1098 19.5878 9.81768H18.0878ZM9.91892 17.8854C5.39925 17.8854 1.75 14.2653 1.75 9.81768H0.25C0.25 15.1098 4.58702 19.3854 9.91892 19.3854V17.8854ZM1.75 9.81768C1.75 5.37007 5.39925 1.75 9.91892 1.75V0.25C4.58702 0.25 0.25 4.52554 0.25 9.81768H1.75ZM9.91892 1.75C14.4386 1.75 18.0878 5.37007 18.0878 9.81768H19.5878C19.5878 4.52554 15.2508 0.25 9.91892 0.25V1.75ZM23.537 22.4764L16.7734 15.5398L15.6994 16.587L22.463 23.5236L23.537 22.4764Z" fill="#EF1326"/>
+      <path
+         d="M22.463 23.5236C22.7522 23.8202 23.227 23.8262 23.5236 23.537C23.8202 23.2478 23.8262 22.773 23.537 22.4764L22.463 23.5236ZM18.0878 9.81768C18.0878 14.2653 14.4386 17.8854 9.91892 17.8854V19.3854C15.2508 19.3854 19.5878 15.1098 19.5878 9.81768H18.0878ZM9.91892 17.8854C5.39925 17.8854 1.75 14.2653 1.75 9.81768H0.25C0.25 15.1098 4.58702 19.3854 9.91892 19.3854V17.8854ZM1.75 9.81768C1.75 5.37007 5.39925 1.75 9.91892 1.75V0.25C4.58702 0.25 0.25 4.52554 0.25 9.81768H1.75ZM9.91892 1.75C14.4386 1.75 18.0878 5.37007 18.0878 9.81768H19.5878C19.5878 4.52554 15.2508 0.25 9.91892 0.25V1.75ZM23.537 22.4764L16.7734 15.5398L15.6994 16.587L22.463 23.5236L23.537 22.4764Z"
+         fill="#EF1326"
+      />
    </svg>
 );
 
 export const IconProfile = (
    <svg width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-   <path d="M20 23V20.5556C20 17.8555 17.8734 15.6667 15.25 15.6667H5.75C3.12665 15.6667 1 17.8555 1 20.5556V23M15.25 5.88889C15.25 8.58895 13.1234 10.7778 10.5 10.7778C7.87665 10.7778 5.75 8.58895 5.75 5.88889C5.75 3.18883 7.87665 1 10.5 1C13.1234 1 15.25 3.18883 15.25 5.88889Z" stroke="#EF1326" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <path
+         d="M20 23V20.5556C20 17.8555 17.8734 15.6667 15.25 15.6667H5.75C3.12665 15.6667 1 17.8555 1 20.5556V23M15.25 5.88889C15.25 8.58895 13.1234 10.7778 10.5 10.7778C7.87665 10.7778 5.75 8.58895 5.75 5.88889C5.75 3.18883 7.87665 1 10.5 1C13.1234 1 15.25 3.18883 15.25 5.88889Z"
+         stroke="#EF1326"
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
    </svg>
 );
 
 export const IconSandwichMobile = (
-    <svg width="28" height="19" viewBox="0 0 28 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_1038_4036)">
-    <path d="M0 4.16992H28.0325" stroke="#EF1326"/>
-    <path d="M0 9.25391H28.0325" stroke="#EF1326"/>
-    <path d="M0 14.3398H28.0325" stroke="#EF1326"/>
-    </g>
-    <defs>
-    <clipPath id="clip0_1038_4036">
-    <rect width="28" height="19" fill="white"/>
-    </clipPath>
-    </defs>
-    </svg>    
-); 
+   <svg width="28" height="19" viewBox="0 0 28 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_1038_4036)">
+         <path d="M0 4.16992H28.0325" stroke="#EF1326" />
+         <path d="M0 9.25391H28.0325" stroke="#EF1326" />
+         <path d="M0 14.3398H28.0325" stroke="#EF1326" />
+      </g>
+      <defs>
+         <clipPath id="clip0_1038_4036">
+            <rect width="28" height="19" fill="white" />
+         </clipPath>
+      </defs>
+   </svg>
+);
 
 export const IconClose = (
-<svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<line x1="0.707031" y1="12.3137" x2="12.0207" y2="0.999997" stroke="#EF1326" stroke-linecap="square"/>
-<line x1="0.707107" y1="1" x2="12.0208" y2="12.3137" stroke="#EF1326" stroke-linecap="square"/>
-</svg>
-); 
+   <svg width="13" height="14" viewBox="0 0 13 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <line
+         x1="0.707031"
+         y1="12.3137"
+         x2="12.0207"
+         y2="0.999997"
+         stroke="#EF1326"
+         strokeLinecap="square"
+      />
+      <line
+         x1="0.707107"
+         y1="1"
+         x2="12.0208"
+         y2="12.3137"
+         stroke="#EF1326"
+         strokeLinecap="square"
+      />
+   </svg>
+);
 
 export const IconArrowDown = (
-<svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.875 11.25L13.5 16.875L19.125 11.25" stroke="#7E133D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-); 
+   <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M7.875 11.25L13.5 16.875L19.125 11.25"
+         stroke="#7E133D"
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
+);
 
 export const IconArrowDownMobile = (
-<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M6.125 8.75L10.5 13.125L14.875 8.75" stroke="#7E133D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-); 
+   <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M6.125 8.75L10.5 13.125L14.875 8.75"
+         stroke="#7E133D"
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
+);
 
 export const IconArrowUp = (
-<svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M19.125 15.75L13.5 10.125L7.875 15.75" stroke="#7E133D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-); 
+   <svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M19.125 15.75L13.5 10.125L7.875 15.75"
+         stroke="#7E133D"
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
+);
 
 export const IconArrowUpMobile = (
-<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14.875 12.25L10.5 7.875L6.125 12.25" stroke="#7E133D" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+   <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M14.875 12.25L10.5 7.875L6.125 12.25"
+         stroke="#7E133D"
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
 );
 
 export const IconReviews = (
-<svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M9 0L10.7986 6.52441L17.5595 6.21885L11.9102 9.94559L14.2901 16.2812L9 12.06L3.70993 16.2812L6.08977 9.94559L0.440492 6.21885L7.20138 6.52441L9 0Z" fill="#7E133D"/>
-</svg>
-); 
+   <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M9 0L10.7986 6.52441L17.5595 6.21885L11.9102 9.94559L14.2901 16.2812L9 12.06L3.70993 16.2812L6.08977 9.94559L0.440492 6.21885L7.20138 6.52441L9 0Z"
+         fill="#7E133D"
+      />
+   </svg>
+);
 
 export const IconReviewsMobile = (
-<svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M7.48077 0L8.97578 5.43701L14.5954 5.18237L9.89974 8.28799L11.8779 13.5676L7.48077 10.05L3.08368 13.5676L5.06179 8.28799L0.366135 5.18237L5.98576 5.43701L7.48077 0Z" fill="#7E133D"/>
-</svg>
-); 
+   <svg width="15" height="14" viewBox="0 0 15 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+         d="M7.48077 0L8.97578 5.43701L14.5954 5.18237L9.89974 8.28799L11.8779 13.5676L7.48077 10.05L3.08368 13.5676L5.06179 8.28799L0.366135 5.18237L5.98576 5.43701L7.48077 0Z"
+         fill="#7E133D"
+      />
+   </svg>
+);
 
 export const IconSliderIndicatorFilled = (
-<svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="4" cy="4" r="3.4" fill="#7E133D" stroke="#7E133D" stroke-width="1.2"/>
-</svg>
-); 
+   <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="4" cy="4" r="3.4" fill="#7E133D" stroke="#7E133D" strokeWidth="1.2" />
+   </svg>
+);
 
 export const IconSliderIndicator = (
-<svg width="9" height="8" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-<circle cx="4.69238" cy="4" r="3.4" stroke="#7E133D" stroke-width="1.2"/>
-</svg>
-); 
-
+   <svg width="9" height="8" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="4.69238" cy="4" r="3.4" stroke="#7E133D" strokeWidth="1.2" />
+   </svg>
+);
